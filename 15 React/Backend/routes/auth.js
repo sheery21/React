97ea673d466e8731b2aae123f5197b3ce7b2312e
sheharyar Hussian <sherypagal123@gmail.com>
2026-01-16 22:57:52 +0500

    Add admin signup and login controllers and routes
    Introduced signUpWithAdminController and logInWithAdminController in the auth controller to handle admin-specific signup and login flows, including email verification with OTP. Updated auth routes to register these new endpoints.

--- 15 React/Backend/routes/auth.js
+++ 15 React/Backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
   logInController,
+  logInWithAdminController,
   resendOTPController,
   signUpController,
+  signUpWithAdminController,
   verifyOTPController,
 } from "../controllers/auth.js";
 
@@ -10,5 +12,7 @@ export const authRoute = express.Router();
 
 authRoute.post("/signUp", signUpController);
 authRoute.post("/logIn", logInController);
+authRoute.post("/signUp", signUpWithAdminController);
+authRoute.post("/logIn", logInWithAdminController);
 authRoute.post("/verify-otp", verifyOTPController);
 authRoute.post("/resend-otp", resendOTPController);
